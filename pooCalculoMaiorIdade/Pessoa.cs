45b574b4fc95ebce1d4aa0dc8c5833bbdd09d8ed
Bernardo <bernardoapelidos@gmail.com>
2025-07-25 16:06:15 -0300

    Programa quase pronto,há melhorias a serem feitas

---
+++ pooCalculoMaiorIdade/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculoMaiorIdade
+{
+	class Pessoa
+	{
+		public Pessoa() { nome = "";idade = 0; }
+		public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+           this.idade = idade;
+        }
+        private int idade;
+
+		public int Idade
+		{
+			get { return idade; }
+			set { idade = value; }
+		}
+
+		private string nome;
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		public void exibirDadosMaior() { Console.WriteLine( Nome + " é o mais velho(a) " + "com a idade de " + idade + " anos"); }
+        
+
+    }
+}
---
+++ pooCalculoMaiorIdade/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculoMaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] pessoas = new Pessoa[3];
+            for (int i = 0; i < 3; i++)
+            {
+                pessoas[i] = new Pessoa();
+                Console.WriteLine("Digite seu nome: ");
+                pessoas[i].Nome = Console.ReadLine();
+                Console.WriteLine("Digite sua idade: ");
+                pessoas[i].Idade = int.Parse(Console.ReadLine());
+
+
+
+            }
+
+            int IndiceMaior = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (pessoas[i].Idade > pessoas[IndiceMaior].Idade)
+                {
+                    IndiceMaior = i;
+                }
+
+            }
+
+            
+            pessoas[IndiceMaior].exibirDadosMaior();
+        }
+    }
+}
